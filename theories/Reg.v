a80b518194f48e036bae501e39e16151c2bcb60b
Karl Palmskog <palmskog@gmail.com>
2025-11-03 13:38:47 +0100

    generalize Reg definition

--- theories/Reg.v
+++ theories/Reg.v
@@ -3,12 +3,10 @@
     "A Play on Regular Expressions" — Fischer, Huch, and Wilke (ICFP 2010).
 *)
 
-From Stdlib Require Import String Ascii.
-
 (* Regular expressions *)
-Inductive Reg : Type :=
-| Eps : Reg                           (* ε *)
-| Sym : Ascii.ascii -> Reg            (* a *)
-| Alt : Reg -> Reg -> Reg             (* α | β *)
-| Seq : Reg -> Reg -> Reg             (* α β *)
-| Rep : Reg -> Reg.                   (* α* *)
+Inductive Reg (A : Type) : Type :=
+| Eps : Reg A                           (* ε *)
+| Sym : A -> Reg A            (* a *)
+| Alt : Reg A -> Reg A -> Reg A             (* α | β *)
+| Seq : Reg A -> Reg A -> Reg A             (* α β *)
+| Rep : Reg A -> Reg A.                   (* α* *)
