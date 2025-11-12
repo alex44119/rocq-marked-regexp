d01ff699ae54d2c766d88a4dc32aed4d4f328dad
Alex Martinez Rivera <alexmr@kth.se>
2025-11-12 18:37:07 +0100

    I finally managed to create the Fixpoint version of `language_of` that should be more proof-friendly.
    Inductive verion is still available but under comments for now.

--- theories/Reg.v
+++ theories/Reg.v
@@ -17,8 +17,39 @@ Inductive Reg (A : Type) : Type :=
 Definition RegChar := Reg Ascii.ascii.
 Definition RegBool := Reg bool.
 
+(* Fixpoint version *)
+Fixpoint size {A : Type} (r : Reg A) : nat :=
+  match r with
+  | Eps _ => 1
+  | Sym _ _ => 1
+  | Alt _ r1 r2 => 1 + size r1 + size r2
+  | Seq _ r1 r2 => 1 + size r1 + size r2
+  | Rep _ r' => 1 + size r'
+  end.
+
+Definition measure {A : Type} (p : Reg A * list A) : nat :=
+  size (fst p) + length (snd p).
+
+Fixpoint language_of {A : Type} (r : Reg A) (w : list A) : Prop :=
+  match r with
+  | Eps _ => w = []
+  | Sym _ a => w = [a]
+  | Alt _ r1 r2 => language_of r1 w \/ language_of r2 w
+  | Seq _ r1 r2 =>
+      exists w1 w2,
+        w = w1 ++ w2 /\
+        language_of r1 w1 /\
+        language_of r2 w2
+  | Rep _ r' =>
+      exists n (ws : list (list A)),
+        (* partition en n sous-mots non vides *)
+        length ws = n /\
+        concat ws = w /\
+        (forall wi, In wi ws -> language_of r' wi)
+  end.
+
 (* Inductive version *)
-Inductive language_of {A : Type} : Reg A -> list A -> Prop :=
+(* Inductive language_of {A : Type} : Reg A -> list A -> Prop :=
 | LangEps : forall w, w = [] -> language_of (Eps A) w
 | LangSym : forall a, language_of (Sym A a) [a]
 | LangAltL : forall r1 r2 w, language_of r1 w -> language_of (Alt A r1 r2) w
@@ -32,44 +63,4 @@ Inductive language_of {A : Type} : Reg A -> list A -> Prop :=
     w1 <> [] ->
     language_of r w1 ->
     language_of (Rep A r) w2 ->
-    language_of (Rep A r) (w1 ++ w2).
-
-
-(* Fixpoint version : doesn't work yet *)
-(* Fixpoint size {A : Type} (r : Reg A) : nat :=
-  match r with
-  | Eps _ => 1
-  | Sym _ _ => 1
-  | Alt _ r1 r2 => 1 + size r1 + size r2
-  | Seq _ r1 r2 => 1 + size r1 + size r2
-  | Rep _ r' => 1 + size r'
-  end.
-
-Definition measure {A : Type} (p : Reg A * list A) : nat :=
-  size (fst p) + length (snd p).
-
-Program Fixpoint language_of {A : Type} (r : Reg A) (w : list A) {measure (size r + length w)} : Prop :=
-  match r, w with
-  | Eps _, _ => w = []
-  | Sym _ a, _ => w = [a]
-  | Alt _ r1 r2, _ => language_of r1 w \/ language_of r2 w
-  | Seq _ r1 r2, _ =>
-      exists w1 w2, w = w1 ++ w2 /\ language_of r1 w1 /\ language_of r2 w2
-  | Rep _ r', _ =>
-      w = [] \/
-      exists w1 w2,
-        w = w1 ++ w2 /\
-        w1 <> [] /\
-        language_of r' w1 /\
-        language_of r w2
-  end.
-Next Obligation.
-simpl.
-lia.
-Qed.
-Next Obligation.
-simpl.
-lia.
-Qed.
-Next Obligation. *)
-
+    language_of (Rep A r) (w1 ++ w2). *)
