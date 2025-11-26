e186d530aafb02bd4488c7ab86926c08aeb4c155
Karl Palmskog <palmskog@gmail.com>
2025-11-26 16:06:31 +0100

    generalize language_of_ind

--- theories/Reg.v
+++ theories/Reg.v
@@ -50,7 +50,7 @@ Fixpoint language_of (r : Reg) (w : list A) : Prop :=
   end.
 
 Inductive language_of_ind : Reg -> list A -> Prop :=
-| LangEps : forall w, w = [] -> language_of_ind Eps w
+| LangEps : language_of_ind Eps []
 | LangSym : forall a, language_of_ind (Sym a) [a]
 | LangAltL : forall r1 r2 w, language_of_ind r1 w -> language_of_ind (Alt r1 r2) w
 | LangAltR : forall r1 r2 w, language_of_ind r2 w -> language_of_ind (Alt r1 r2) w
@@ -60,7 +60,7 @@ Inductive language_of_ind : Reg -> list A -> Prop :=
     language_of_ind (Seq r1 r2) (w1 ++ w2)
 | LangRep0 : forall r, language_of_ind (Rep r) []
 | LangRepStep : forall r w1 w2,
-    w1 <> [] -> language_of_ind r w1 ->
+    language_of_ind r w1 ->
     language_of_ind (Rep r) w2 ->
     language_of_ind (Rep r) (w1 ++ w2).
 
@@ -78,34 +78,58 @@ Proposition language_of_to_ind :
     language_of r w -> language_of_ind r w.
 Proof.
   intros r. induction r.
-  - intros. unfold language_of in H. rewrite H. apply LangEps. reflexivity.
+  - intros. unfold language_of in H. rewrite H. apply LangEps.
   - intros. unfold language_of in H. rewrite H. apply LangSym.
   - intros. destruct H.
     * apply LangAltL. apply IHr1. assumption.
     * apply LangAltR. apply IHr2. assumption.
-- intros. destruct H as [l1 [l2 [concat [H H']]]].
+  - intros. destruct H as [l1 [l2 [concat [H H']]]].
     apply IHr1 in H. apply IHr2 in H'. 
     rewrite concat. apply LangSeq. 
     assumption. assumption. 
   - intros. destruct H as [x [H]]. rewrite <- H. clear H w. 
-  induction x.
+    induction x as [H|a l].
     * rewrite concat_nil. apply LangRep0.
-    * pose proof ((IHr a) ((H0 a) (in_eq a x))). 
-    assert (forall wi : list A, In wi x -> language_of r wi).
-      + intros. pose proof ((H0 wi) ((in_cons a wi x) H1)). assumption.
-      + pose proof (IHx H1). clear IHx H1. destruct a.
-       -- assumption.
-       -- assert ([] <> a :: a0) as Hnil by (apply nil_cons).
-          apply ((LangRepStep r (a::a0) (concat x)) (not_eq_sym Hnil) H H2).
+    * simpl; apply LangRepStep.
+      + apply IHr; apply H0; left; reflexivity.
+      + apply IHl.
+        intros wi Hwi.
+        apply H0; right; assumption.
 Qed.
 
+Lemma language_of_ind_Rep_exists' :
+forall r (w : list A), language_of_ind r w ->
+ forall r', r = Rep r' ->
+  w <> [] ->
+  exists w1 w2, w = w1 ++ w2 /\ w1 <> [] /\ language_of_ind r' w1 /\ language_of_ind (Rep r') w2.
+Proof.
+induction 1; intros; try discriminate.
+- contradict H0; reflexivity.
+- inversion H1; subst.
+  destruct w1.
+  * assert (Heq: Rep r' = Rep r') by auto.
+    specialize (IHlanguage_of_ind2 _ Heq H2).
+    assumption.
+  * exists (a::w1), w2.
+    split; try reflexivity.
+    split; try discriminate.
+    split; assumption.
+Qed.
+
+Lemma language_of_ind_Rep_exists :
+forall r (w : list A), language_of_ind (Rep r) w ->
+  w <> [] ->
+  exists w1 w2, w = w1 ++ w2 /\ w1 <> [] /\ language_of_ind r w1 /\ language_of_ind (Rep r) w2.
+Proof.
+intros; eapply language_of_ind_Rep_exists'; eauto.
+Qed.
 
 Lemma language_of_to_fix :
   forall (r : Reg) (w : list A),
     language_of_ind r w -> language_of r w.
 Proof.
   intros r. induction r. 
-  - intros. dependent destruction H. assumption.
+  - intros. inversion H; subst. reflexivity.
   - intros. dependent destruction H. simpl. reflexivity.
   - intros. dependent destruction H. simpl. 
       left.  apply ((IHr1 w) H).
@@ -119,20 +143,34 @@ Proof.
     + exists []. split.
       * apply concat_nil.
       * intros. inversion H0.
-    + dependent destruction H.
+    + destruct w.
       * exists []. split.
         -- apply concat_nil.
-        -- intros. inversion H.
-      * rewrite <- length_zero_iff_nil in H. apply not_eq_sym in H.
-        apply (Arith_base.neq_0_lt_stt (Datatypes.length w1)) in H.
-        assert (length w2 < length w1 + length w2) by lia. rewrite <- (length_app w1 w2) in H3.
-        pose proof (H0 w2 H3 H2). destruct H4. exists (w1 :: x). split.
-        -- rewrite concat_cons. destruct H4. rewrite H4. reflexivity. 
-        -- destruct H4. intros. destruct H6.
-          ++ rewrite <- H6. apply H1.
-          ++ apply (H5 wi H6).
+        -- intros; inversion H1.
+      * assert (Haw: a :: w <> []) by auto with datatypes.
+        pose proof (language_of_ind_Rep_exists _ _ H Haw)
+         as [w1 [w2 [Heq [Hneq [Hw Hw']]]]].
+        assert (Hw2: length w2 < length (a :: w)). {
+          destruct w1.
+          - contradict Hneq; reflexivity.
+          - simpl in Heq.
+            inversion Heq; subst.
+            simpl.
+            rewrite length_app.
+            lia.
+        }
+        pose proof (H0 w2 Hw2 Hw') as [ws [Hws]].
+        rewrite Heq.
+        exists (w1 :: ws).
+        simpl.
+        rewrite Hws.
+        split; [reflexivity|].
+        intros wi Hin.
+        destruct Hin.
+        -- rewrite <- H2; assumption.
+        -- apply H1; assumption.
     + simpl. repeat destruct H0. clear H. exists x. split. reflexivity. intros. 
-    apply ((IHr wi) (H1 wi H)).
+      apply ((IHr wi) (H1 wi H)).
 Qed.
 
 Theorem language_of_equiv :
